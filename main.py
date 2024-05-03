8c35a10342cc6efd0d065afd44b22f3add149270
DoktorTomato <96630602+DoktorTomato@users.noreply.github.com>
2024-05-04 00:35:35 +0300

    The working enviroment was set up and basic stuf done. Now window can be created (temporarly with nothing in it)

---
+++ main.py
@@ -0,0 +1,34 @@
+'''
+This is the main file that has script to start the app
+'''
+import sys
+import pygame
+import settings
+
+class Game:
+    '''
+    This is the game class which is responsible with initializing a game screen 
+    and starting a game itself
+    '''
+    def __init__(self) -> None:
+        pygame.init()
+        pygame.display.set_caption('Da game')
+        self.screen = pygame.display.set_mode((settings.WIDTH, settings.HEIGHT))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        '''
+        This method is responsible for creating and running a window
+        '''
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            self.screen.fill(settings.WHITE)
+            pygame.display.update()
+            self.clock.tick(settings.FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
---
+++ settings.py
@@ -0,0 +1,13 @@
+'''
+This is a file that contains the variables relieble for settings of some sort
+'''
+WIDTH = 1280
+HEIGHT = 720
+FPS = 60
+TILESIZE = 12
+
+#Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+UCU_MAP = []
