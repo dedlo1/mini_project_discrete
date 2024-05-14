f87cc593290c7549a34d264a21f01e054a1e4d77
DoktorTomato <96630602+DoktorTomato@users.noreply.github.com>
2024-05-14 03:19:11 +0300

    Updatae pan Stepan event and added visualisation for Oles event

--- ivents.py
+++ ivents.py
@@ -1,6 +1,14 @@
+import pygame
 import settings
+import random
 from student_class import Park, Student, Canteene
+from special_units_waypoints import (StepanWaypoint1, StepanWaypoint2, StepanWaypoint3,
+                                  StepanWaypoint4, StepanWaypoint5, StepanWaypoint6,
+                                  StepanWaypoint7, StepanWaypoint8, StepanWaypoint9,
+                                  OlesWaypoint1)
 
+TIMER_TEMPLATE = [False]*25
+TIMER_TEMPLATE.append(True)
 
 class Ivents:
     """
@@ -35,15 +43,15 @@ class Ivents:
         Increase chance_to_fail by 7
         """
         for student in student_set:
-            student.will_to_live -= 5
             student.chance_to_fail -= 5
 
     @staticmethod
-    def Oles(student_set):
+    def Oles(student_set, coords):
         """
         Increase will_to_live by 5
         """
         for student in student_set:
+            student.dest = coords
             student.will_to_live += 5
 
     @staticmethod
@@ -64,31 +72,46 @@ class Ivents:
             if student.chance_to_fail >= 40:
                 student_set.remove(student)
 
-import random
-from student_class import Student
-
-
 class Pan_S(Student):
     def __init__(self, coords):
         super().__init__(50, 0, coords)
 
     def move(self):
 
-
         if self.dest is None:
             self.set_new_destination(Park())
-
-
-        elif self.boredom >= 10:
-            self.set_new_destination(Canteene())
-            self.boredom = 0
 
         elif self.coords in self.dest:
 
             if isinstance(self.dest, Park):
-                self.move_inside()
-                self.boredom += 1
+                self.set_new_destination(StepanWaypoint1())
 
+            elif isinstance(self.dest, StepanWaypoint1):
+                self.set_new_destination(StepanWaypoint2())
+
+            elif isinstance(self.dest, StepanWaypoint2):
+                self.set_new_destination(StepanWaypoint3())
+
+            elif isinstance(self.dest, StepanWaypoint3):
+                self.set_new_destination(StepanWaypoint4())
+
+            elif isinstance(self.dest, StepanWaypoint4):
+                self.set_new_destination(StepanWaypoint5())
+
+            elif isinstance(self.dest, StepanWaypoint5):
+                self.set_new_destination(StepanWaypoint6())
+
+            elif isinstance(self.dest, StepanWaypoint6):
+                self.set_new_destination(StepanWaypoint7())
+
+            elif isinstance(self.dest, StepanWaypoint7):
+                self.set_new_destination(StepanWaypoint8())
+
+            elif isinstance(self.dest, StepanWaypoint8):
+                self.set_new_destination(StepanWaypoint9())
+
+            elif isinstance(self.dest, StepanWaypoint9):
+                self.set_new_destination(Canteene())
 
             elif isinstance(self.dest, Canteene):
                 return True
@@ -100,4 +123,42 @@ class Pan_S(Student):
 
 
     def __contains__(self, coords):
-        return self.coords[0] <= coords[0] <= self.coords[0] + 2 and self.coords[1] <= coords[1] <= self.coords[1] + 2
+        return (self.coords[0] <= coords[0] <= self.coords[0] + 2 and
+                self.coords[1] <= coords[1] <= self.coords[1] + 2)
+
+class Oles(Student):
+    def __init__(self, coords, screen):
+        super().__init__(50, 0, coords)
+        self.screen = screen
+        self.timer = iter(TIMER_TEMPLATE)
+
+    def start_picnic(self):
+        pygame.draw.rect(self.screen, (242, 215, 61),
+                            ((self.coords[1]-2)*settings.TILESIZE, (self.coords[0]-2)*settings.TILESIZE,
+                            settings.TILESIZE*6, settings.TILESIZE*6))
+
+    def move(self):
+
+        if self.dest is None:
+            self.set_new_destination(OlesWaypoint1())
+
+        elif self.coords in self.dest:
+
+            if isinstance(self.dest, OlesWaypoint1):
+                self.start_picnic()
+                if next(self.timer):
+                    self.set_new_destination(Canteene())
+                # self.set_new_destination(Canteene())
+
+            elif isinstance(self.dest, Canteene):
+                return True
+
+        if not self.path:
+            self.grid_bfs()
+
+        self.coords = self.path.pop(0)
+
+
+    def __contains__(self, coords):
+        return (self.coords[0] <= coords[0] <= self.coords[0] + 2 and
+                self.coords[1] <= coords[1] <= self.coords[1] + 2)
---
+++ special_units_waypoints.py
@@ -0,0 +1,114 @@
+'''
+This is script contains classes for Pan Stepan road.
+It's a bruteforce way to do it but it's fine
+'''
+
+class StepanWaypoint1:
+    '''
+    This is a class of Waypoint for Pan Stepan event
+    '''
+    def __init__(self):
+        self.will_to_live = 0
+        self.chance_to_fail = 0
+
+    def __contains__(self, coords):
+        return 32 <= coords[0] <= 34 and 58 <= coords[1] <= 60
+
+class StepanWaypoint2:
+    '''
+    This is a class of Waypoint for Pan Stepan event
+    '''
+    def __init__(self):
+        self.will_to_live = 0
+        self.chance_to_fail = 0
+
+    def __contains__(self, coords):
+        return 31 <= coords[0] <= 34 and 66 <= coords[1] <= 68
+
+class StepanWaypoint3:
+    '''
+    This is a class of Waypoint for Pan Stepan event
+    '''
+    def __init__(self):
+        self.will_to_live = 0
+        self.chance_to_fail = 0
+
+    def __contains__(self, coords):
+        return 17 <= coords[0] <= 19 and 65 <= coords[1] <= 67
+
+class StepanWaypoint4:
+    '''
+    This is a class of Waypoint for Pan Stepan event
+    '''
+    def __init__(self):
+        self.will_to_live = 0
+        self.chance_to_fail = 0
+
+    def __contains__(self, coords):
+        return 15 <= coords[0] <= 16 and 52 <= coords[1] <= 53
+
+class StepanWaypoint5:
+    '''
+    This is a class of Waypoint for Pan Stepan event
+    '''
+    def __init__(self):
+        self.will_to_live = 0
+        self.chance_to_fail = 0
+
+    def __contains__(self, coords):
+        return 24 <= coords[0] <= 25 and 52 <= coords[1] <= 53
+
+class StepanWaypoint6:
+    '''
+    This is a class of Waypoint for Pan Stepan event
+    '''
+    def __init__(self):
+        self.will_to_live = 0
+        self.chance_to_fail = 0
+
+    def __contains__(self, coords):
+        return 24 <= coords[0] <= 25 and 41 <= coords[1] <= 42
+
+class StepanWaypoint7:
+    '''
+    This is a class of Waypoint for Pan Stepan event
+    '''
+    def __init__(self):
+        self.will_to_live = 0
+        self.chance_to_fail = 0
+
+    def __contains__(self, coords):
+        return 9 <= coords[0] <= 10 and 41 <= coords[1] <= 42
+
+class StepanWaypoint8:
+    '''
+    This is a class of Waypoint for Pan Stepan event
+    '''
+    def __init__(self):
+        self.will_to_live = 0
+        self.chance_to_fail = 0
+
+    def __contains__(self, coords):
+        return 8 <= coords[0] <= 9 and 17 <= coords[1] <= 18
+
+class StepanWaypoint9:
+    '''
+    This is a class of Waypoint for Pan Stepan event
+    '''
+    def __init__(self):
+        self.will_to_live = 0
+        self.chance_to_fail = 0
+
+    def __contains__(self, coords):
+        return 17 <= coords[0] <= 18 and 17 <= coords[1] <= 18
+
+class OlesWaypoint1:
+    '''
+    This is a class of Waypoint for Pan Stepan event
+    '''
+    def __init__(self):
+        self.will_to_live = 0
+        self.chance_to_fail = 0
+
+    def __contains__(self, coords):
+        return 3 <= coords[0] <= 4 and 19 <= coords[1] <= 20
